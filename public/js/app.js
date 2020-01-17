0063842c5b3b319f0958681a612f0520116a7e4e
Fred Shaw-Matthews <frederick@shaw-matthews.co.uk>
2020-01-17 15:18:24 +0000

    header section

--- public/js/app.js
+++ public/js/app.js
@@ -19287,8 +19287,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\fredshaw-matthews\Documents\Projects\natours\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\fredshaw-matthews\Documents\Projects\natours\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! c:\Users\fredshaw-matthews\Documents\Projects\my-sites\natours\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! c:\Users\fredshaw-matthews\Documents\Projects\my-sites\natours\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
